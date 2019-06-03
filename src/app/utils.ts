e93a43054fb7f61338f076c3f49361af977590c8
rhn <rhnh@protonmail.com>
2019-06-03 14:57:18 +0200

    refactoring and adding small changes

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,7 +1,7 @@
+/**
+ * π/4 = 1/1 - 1/3 + 1/5 - 1 /7 + 1/9 ± ...
+ */
 export function LeibnizSeries(value: number): number {
-  /**
-   * π/4 = 1/1 - 1/3 + 1/5 - 1 /7 + 1/9 ± ...
-   */
   let sum: number = 0;
 
   for (let i: number = 1; i < 2 * value; i++) {
@@ -16,10 +16,25 @@ export function LeibnizSeries(value: number): number {
   return (sum *= 4);
 }
 
+/**
+ *  e^x = 1 + x + x^2/2! + x^3/3! + x^4/4! + x^5/5! +... + x^n/n!
+ * @param x number
+ * @param n number
+ */
+export function exponentialSeries(x: number, n: number): number {
+  let result: number = 1;
+  let e: number = 1;
+  for (let i: number = 1; i <= n; i++) {
+    result *= x / i; // the numerator constant (x) and the  denominator variable (i) will be multiplied
+    e += result;
+  }
+  return e;
+}
+
+/**
+ * 1+3+5....
+ */
 export function sumOfOddNumbers(value: number): number {
-  /**
-   * 1+3+5....
-   */
   let sum: number = 0;
   for (let i: number = 0; i <= value; i++) {
     if (oddNumber(i)) {
@@ -28,16 +43,35 @@ export function sumOfOddNumbers(value: number): number {
   }
   return sum;
 }
+/**
+ * O(sqrt(n))
+ * @param value number
+ */
+export function isPrime(value: number): boolean {
+  if (value <= 1) return false;
+  if (value <= 3) return true; // 2 or 3 => true
 
+  if (value % 2 === 0 || value % 3 === 0) {
+    // can be divided by 2 or 3 => false
+    return false;
+  }
+  for (let i = 5; i * i <= value; i = i + 6) {
+    //value >= 25 ?
+    if (value % i === 0 || value % (i + 2) === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/**
+ * 1 + 1/2+ 1/3 + 1/4 ...
+ */
 export function harmonicSeries(value: number): number {
-  /**
-   * 1 + 1/2+ 1/3 + 1/4 ...
-   */
   let sum: number = 0;
   for (let i: number = 1; i <= value; i++) {
     sum += 1 / i;
   }
-
   return sum;
 }
 
@@ -48,6 +82,7 @@ function oddNumber(value: number): boolean {
   }
   return false;
 }
+
 function evenNumber(value: number): boolean {
   if (value % 2 === 0) {
     return true;
--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,25 @@ const result = document.getElementById("result");
 const harmonicSeriesBtn = document.getElementById("harmonic-series");
 const leibnizSeriesBtn = document.getElementById("leibniz-series");
 const sumOfOddsBtn = document.getElementById("sum-of-odds");
-
+const inputBox = <HTMLInputElement>document.getElementById("input-value");
+const isPrime = document.getElementById("is-prime");
 harmonicSeriesBtn.addEventListener("click", () => {
-  const value = prompt("Please Enter a number");
+  const value = inputBox.value;
   let res: number = utils.harmonicSeries(+value);
   result.innerHTML = res.toString();
 });
 sumOfOddsBtn.addEventListener("click", () => {
-  const value = prompt("Please Enter a number");
+  const value = inputBox.value;
   let res: number = utils.sumOfOddNumbers(+value);
   result.innerHTML = res.toString();
 });
 leibnizSeriesBtn.addEventListener("click", () => {
-  const value = prompt("Please Enter a number");
+  const value = inputBox.value;
   let res: number = utils.LeibnizSeries(+value);
+  result.innerHTML = res.toString();
+});
+isPrime.addEventListener("click", () => {
+  const value = inputBox.value;
+  let res: number = utils.isPrime(+value);
   result.innerHTML = res.toString();
 });
