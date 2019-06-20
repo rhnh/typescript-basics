39190f5ec190ec642cc2518bfee14305ad369566
rhn <rhnh@protonmail.com>
2019-06-20 19:07:23 +0200

    experiment with dom

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -129,3 +129,26 @@ function evenNumber(value: number): boolean {
   }
   return false;
 }
+
+export function addInputBox(inputText: string): HTMLInputElement {
+  let inputBox = document.createElement("input");
+  inputBox.className = "form-name";
+  inputBox.setAttribute("id", "formName");
+  inputBox.placeholder = inputText;
+  return inputBox;
+}
+
+export function addParagraph(
+  elementId: string,
+  desc: string
+): HTMLParagraphElement {
+  let element = <HTMLParagraphElement>document.getElementById(elementId);
+  element.innerText = desc;
+  return element;
+}
+
+export function removeAllChildren(element): void {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,101 @@
 import * as utils from "./app/utils";
 
-const result = document.getElementById("result");
 const harmonicSeriesBtn = document.getElementById("harmonic-series");
 const leibnizSeriesBtn = document.getElementById("leibniz-series");
 const sumOfOddsBtn = document.getElementById("sum-of-odds");
-const inputBox = <HTMLInputElement>document.getElementById("input-value");
 const isPrime = document.getElementById("is-prime");
+const modal = document.getElementById("modal");
+const close = document.getElementById("close");
+const cancel = document.getElementById("cancel");
 const primeFactor = document.getElementById("prime-factor");
+const modalForm = document.getElementById("input-form");
+
 harmonicSeriesBtn.addEventListener("click", () => {
-  const value = inputBox.value;
-  let res: number = utils.harmonicSeries(+value);
-  result.innerHTML = res.toString();
+  let inputBox = utils.addInputBox("This is an inputBox");
+  let calculate = document.getElementById("calculate");
+
+  modalForm.appendChild(inputBox);
+  utils.addParagraph("description", " Harmonic Series ");
+  let parentDiv = document.getElementById("formName").parentNode;
+  calculate.addEventListener("click", () => {
+    let result = document.createElement("p");
+    result.innerText = utils.harmonicSeries(+inputBox.value).toString();
+    parentDiv.insertBefore(result, inputBox);
+  });
+
+  modal.style.display = "block";
 });
+
 sumOfOddsBtn.addEventListener("click", () => {
-  const value = inputBox.value;
-  let res: number = utils.sumOfOddNumbers(+value);
-  result.innerHTML = res.toString();
+  let inputBox = utils.addInputBox("This is an inputBox");
+  let calculate = document.getElementById("calculate");
+
+  modalForm.appendChild(inputBox);
+  utils.addParagraph("description", " Harmonic Series ");
+  let parentDiv = document.getElementById("formName").parentNode;
+  calculate.addEventListener("click", () => {
+    let result = document.createElement("p");
+    result.innerText = utils.sumOfOddNumbers(+inputBox.value).toString();
+    parentDiv.insertBefore(result, inputBox);
+  });
+
+  modal.style.display = "block";
 });
+
 leibnizSeriesBtn.addEventListener("click", () => {
-  const value = inputBox.value;
-  let res: number = utils.LeibnizSeries(+value);
-  result.innerHTML = res.toString();
+  let inputBox = utils.addInputBox("This is an inputBox");
+  let calculate = document.getElementById("calculate");
+
+  modalForm.appendChild(inputBox);
+  utils.addParagraph("description", " Harmonic Series ");
+  let parentDiv = document.getElementById("formName").parentNode;
+  calculate.addEventListener("click", () => {
+    let result = document.createElement("p");
+    result.innerText = utils.LeibnizSeries(+inputBox.value).toString();
+    parentDiv.insertBefore(result, inputBox);
+  });
+
+  modal.style.display = "block";
 });
+
 isPrime.addEventListener("click", () => {
-  const value = inputBox.value;
-  let res: boolean = utils.isPrime(+value);
-  result.innerHTML = res.toString();
+  let inputBox = utils.addInputBox("This is an inputBox");
+  let calculate = document.getElementById("calculate");
+
+  modalForm.appendChild(inputBox);
+  utils.addParagraph("description", " Harmonic Series ");
+  let parentDiv = document.getElementById("formName").parentNode;
+  calculate.addEventListener("click", () => {
+    let result = document.createElement("p");
+    result.innerText = utils.isPrime(+inputBox.value).toString();
+    parentDiv.insertBefore(result, inputBox);
+  });
+
+  modal.style.display = "block";
 });
 
 primeFactor.addEventListener("click", () => {
-  const value = inputBox.value;
-  let res: string = utils.primeFactors(+value);
-  result.innerHTML = res;
+  let inputBox = utils.addInputBox("This is an inputBox");
+  let calculate = document.getElementById("calculate");
+
+  modalForm.appendChild(inputBox);
+  utils.addParagraph("description", " Harmonic Series ");
+  let parentDiv = document.getElementById("formName").parentNode;
+  calculate.addEventListener("click", () => {
+    let result = document.createElement("p");
+    result.innerText = utils.primeFactors(+inputBox.value).toString();
+    parentDiv.insertBefore(result, inputBox);
+  });
+
+  modal.style.display = "block";
+});
+
+close.addEventListener("click", () => {
+  modal.style.display = "none";
+  utils.removeAllChildren(modalForm);
+});
+
+cancel.addEventListener("click", () => {
+  modal.style.display = "none";
+  utils.removeAllChildren(modalForm);
 });
